93326d090a8526c1bb049fdd185fe2a405beb29a
Artem Osta <ostart@mail.ru>
2026-05-15 10:56:27 +0300

    Сделай кнопу у менеджера "создать" более явную с иконкой

--- app/Filament/Resources/ManagementResource/Pages/ListManagement.php
+++ app/Filament/Resources/ManagementResource/Pages/ListManagement.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\ManagementResource\Pages;
 use App\Filament\Resources\ManagementResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Support\Enums\IconSize;
+use Filament\Support\Enums\Size;
 
 class ListManagement extends ListRecords
 {
@@ -13,7 +15,12 @@ class ListManagement extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+                ->label('Создать')
+                ->icon('heroicon-o-user-plus')
+                ->iconSize(IconSize::Large)
+                ->size(Size::Large)
+                ->color('primary'),
         ];
     }
 }
