4291c6609f0df3d54cdb8589b84c04b9f11f549e
Artem Osta <ostart@mail.ru>
2026-05-15 08:28:00 +0300

    fix bug

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -22,6 +22,7 @@ class AdminPanelProvider extends PanelProvider
     public function panel(Panel $panel): Panel
     {
         return $panel
+            ->default()
             ->id('admin')
             ->path('admin')
             ->login(\App\Filament\Pages\Auth\Login::class)
